1b166093412c0cbddf1796ba561310a2b2462d87
Conzpite <mtszenson@gmail.com>
2025-03-17 15:12:48 +0800

    feat: Add support for expected rows with multiple dice

--- README.md
+++ README.md
@@ -1,16 +1,38 @@
 # ironvault-oracle-template
-Contains a script to generate a markdown template for homebrew oracles to use in Ironvault Obsidian
+
+This is created as a way for me to generate oracle templates for homebrewing ironvault without typing the whole thing out by hand. Feel free to use as you wish.
 
 ## How to use
 
-For general use cases, you 
+If you intend to only use the pre-generated templates, you can simply download just the Templates folder.
+
+To test using the template folder, 
+
+1. Add the Templates folder to your homebrew directory. Make sure homebrew content is enabled in the ironvault plugins settings
+2. Create a new campaign/Edit an existing campaign
+3. Ensure that the Custom playset is selected
+4. Click on the Configure button
+5. Ensure the Custom Playset is selected here
+6. Add '*:templates/\*\*' to the text field
+7. Click select, then save
+8. Click on any page on the campaign
+9. If you don't have a right pane currently, click on the expand button on the top right of your tabs row (Right beside the minimize button)
+10. Select the IronVault button at the top right (You may have to drag out the right pane to see it), and make sure Oracle is selected
+11. Select 'Templates/Basic Templates'
+12. Click on any of the templates to activate it
+
+Then, whenever you need a template to work off, copy and paste the template you wish to use, renaming the file and adjusting the description and contents accordingly.
 
 
-### Note!: About filenames
-
-
+If you intend to generate your own custom templates, continue below...
 
 ## Generate custom file
+
+Run the generate.py file to generate a custom file. 
+
+If successful, a new file will be writen in the output folder. 
+
+By default, if no name is provided, then the file will default to "t_XdY{_ROWS}", so basically "t_d6", "t_3d10", "t_d100_3" etc...
 
 ### Command
 ```python generate.py [dice_expression]```
@@ -20,19 +42,16 @@ For general use cases, you
 positional arguments:
   dice_expression       Dice notation of what dice the oracle should roll on. Currently supports only dY and XdY
                         format ('d4', '1d6', '2d10' etc)
-```
 
-```
 options:
   -h, --help            Shows this help message and exit
-  -r, --rows [ROWS]     Expected number of results. An non-zero input value here, along with a dice experssion of dY,
+  -r, --rows [ROWS]     Expected number of results. A non-zero input value here, along with a dice experssion of dY,
                         will result in dice values being consolidated in that many rows (a d6 with rows of 3 result in
                         1-2, 3-4, 5-6 columns). In the event that that the values cannot be evenly split, the extra
-                        values will be spread among the rows, starting from the first row. Does nothing for XdY where
-                        X is not 1
+                        values will be spread among the rows, starting from the first row
   -o, --output-file_name [OUTPUT_FILE_NAME]
-                        Name of the generated file (excluding file extension). Note that this must not begin with a
-                        number, as this will cause ironvault to fail reading the file
+                        Name of the generated file (excluding .md file extension). If left blank/not used, will
+                        default to a generated name instead.
   -d, --description [DESCRIPTION]
                         Description to place in resulting oracle file
   -w, --overwrite       Automatically overwrites file without prompting
@@ -45,6 +64,9 @@ options:
                         stop it from doing so
   -p, --print-debug     Set this to allow the system to print messages, mainly for debugging purposes
 ```
+
+### Default file names
+
 
 ## Testing
 Testing is done via pytest, which needs to be installed via pip. 
@@ -60,3 +82,7 @@ It's highly recommanded to run this if you intend to make any changes to the pro
 ### To Run Tests 
 
 ```pytest```
+
+## Addendum: About filenames
+
+**This is relevant as of Ironvault v1.89.4**
--- generate.py
+++ generate.py
@@ -7,8 +7,8 @@ def debug_print(*args):
     if DEBUG_PRINT:
         print(*args);
 
-def expected_rows_is_more_than_dice_sides(expected_rows, dice_sides):
-    return expected_rows > dice_sides;
+def expected_rows_is_more_than_range(expected_rows, range):
+    return expected_rows > range;
 
 def extract_dice_values(dice_expression):
     # Validate dice expression
@@ -47,7 +47,7 @@ def generate_table_values(dice_num, dice_sides, expected_rows, dataset):
             row_range = dice_sides // expected_rows; # How much range each row should cover
             remainder = dice_sides % expected_rows; # How many rows should get the extra leftovers
 
-            if expected_rows_is_more_than_dice_sides(expected_rows, dice_sides):
+            if expected_rows_is_more_than_range(expected_rows, dice_sides):
                 print("More expected rows requested than is possible for dice of side {}".format(dice_sides));
                 print("Changing to {} rows".format(dice_sides));
                 expected_rows = dice_sides;
@@ -74,13 +74,40 @@ def generate_table_values(dice_num, dice_sides, expected_rows, dataset):
                 result_col.append(retrieve_dataset_value(len(result_col), dataset));
     else:
         # Calulate all possible value of the dice as its own row
-        # This is not affected in any way by expected_rows
         min_roll_value = dice_num;
         max_roll_value = dice_num * dice_sides;
+        dice_range = max_roll_value - min_roll_value + 1;
 
-        for x in range(min_roll_value, max_roll_value + 1):
-            dice_values_col.append(str(x));
-            result_col.append(retrieve_dataset_value(len(result_col), dataset));
+        if expected_rows != 0:
+            # Calculate values based on expected rows
+            row_range = dice_range // expected_rows; # How much range each row should cover
+            remainder = dice_range % expected_rows; # How many rows should get the extra leftovers
+
+            if expected_rows_is_more_than_range(expected_rows, dice_range):
+                print("More expected rows requested than is possible for dice of {}d{}".format(dice_num, dice_sides));
+                print("Changing to {} rows".format(dice_range));
+                expected_rows = dice_range;
+                row_range = 1;
+                remainder = 0;
+
+            dice_value = min_roll_value - 1;
+
+            for x in range(expected_rows):
+                min_range = dice_value + 1;
+                max_range = dice_value + row_range + 1 if remainder > x else dice_value + row_range;
+
+                if min_range == max_range:
+                    dice_values_col.append(str(min_range));
+                else:
+                    dice_values_col.append("{}-{}".format(min_range, max_range));
+                result_col.append(retrieve_dataset_value(len(result_col), dataset));
+
+                dice_value = max_range;
+        else:
+            # Each value from 1 to dice_side is its own row
+            for x in range(min_roll_value, max_roll_value + 1):
+                dice_values_col.append(str(x));
+                result_col.append(retrieve_dataset_value(len(result_col), dataset));
 
     return dice_values_col, result_col
 
@@ -138,8 +165,7 @@ def write_to_file(header, body, fileName, filePath='output/', always_overwrite=F
             reply = input("{} already exists. Overwrite file? (y/n)\n".format(full_path));
 
         if reply == "n":
-            print("Operation has ceased...")
-            exit();
+            exit("Operation has ceased...");
         elif reply == "y":
             # No need to do anything 
             pass;
@@ -151,8 +177,7 @@ def write_to_file(header, body, fileName, filePath='output/', always_overwrite=F
             f.write(body);
             print("Writing completed");
     except FileNotFoundError:
-        print("Unable to write to {}".format(full_path));
-        exit()
+        exit("Unable to write to {}".format(full_path))
 
 def read_dataset(file_content, delimiter, auto_trim):
     dataset_values = file_content.split(delimiter);
@@ -173,36 +198,39 @@ def read_dataset_from_file(filepath, delimiter, auto_trim):
 
             return read_dataset(file_content, delimiter, auto_trim)
     except FileNotFoundError:
-        print("File not found. Check the path variable and filename")
-        exit()
+        exit("File not found. Check the path variable and filename")
 
 def create_default_filename(dice_expression, expected_rows):
     [dice_num, dice_sides] = extract_dice_values(dice_expression);
 
-    prefix = "t-";
+    prefix = "t_";
 
     filename = "{}{}d{}".format(prefix, dice_num if dice_num != 1 else "", dice_sides);
 
-    if expected_rows_is_more_than_dice_sides(expected_rows, dice_sides):
+    dice_range = dice_sides if dice_num == 1 else (dice_num * dice_sides - dice_num + 1)
+
+    if expected_rows_is_more_than_range(expected_rows, dice_range):
         # rows will be decreased to meet sides in this scenerio, so no need to append rows
         pass;
-    elif dice_num != 1:
-        # expected rows has no use with multiple dice, so no need to append rows
-        pass;
     elif expected_rows != 0:
-        filename += "-{}".format(expected_rows);
+        filename += "_{}".format(expected_rows);
 
     return filename;
+
+def validateFileName(output_name):
+    if output_name[0].isdigit() :
+        # Need to prevent output file from starting with a digit
+        exit("Output file must not begin with a number. Please change the output file name and try again.");
 
 
 def main():
     parser = argparse.ArgumentParser("Generate a .md file to serve as a homebrew oracle for iron vault obsidian")
 
     parser.add_argument("dice_expression", help="Dice notation of what dice the oracle should roll on. Currently supports only dY and XdY format ('d4', '1d6', '2d10' etc)", type=str)
-    parser.add_argument("-r", "--rows", nargs="?", default=0, const=0, help="Expected number of results. An non-zero input value here, along with a dice experssion of dY, will result in dice values being consolidated in that many rows (a d6 with rows of 3 result in 1-2, 3-4, 5-6 columns). In the event that that the values cannot be evenly split, the extra values will be spread among the rows, starting from the first row. Does nothing for XdY where X is not 1", type=int)
+    parser.add_argument("-r", "--rows", nargs="?", default=0, const=0, help="Expected number of results. A non-zero input value here, along with a dice experssion of dY, will result in dice values being consolidated in that many rows (a d6 with rows of 3 result in 1-2, 3-4, 5-6 columns). In the event that that the values cannot be evenly split, the extra values will be spread among the rows, starting from the first row", type=int)
 
     # Output file related
-    parser.add_argument("-o", "--output-file_name", nargs="?", default="", const="", help="Name of the generated file (excluding file extension). Note that this must not begin with a number, as this will cause ironvault to fail reading the file", type=str)
+    parser.add_argument("-o", "--output-file_name", nargs="?", default="", const="", help="Name of the generated file (excluding .md file extension). If left blank/not used, will default to a generated name instead", type=str)
     parser.add_argument("-d", "--description", nargs="?", default="Here is a description of my oracle", const="Here is a descripton of my oracle", help="Description to place in resulting oracle file",  type=str)
     parser.add_argument("-w", "--overwrite", action="store_true", help="Automatically overwrites file without prompting")
 
@@ -213,6 +241,7 @@ def main():
 
     # Debug related
     parser.add_argument("-p", "--print-debug", action="store_true", help="Set this to allow the system to print messages, mainly for debugging purposes")
+
 
     args = parser.parse_args()
 
@@ -226,12 +255,7 @@ def main():
         output_name = create_default_filename(args.dice_expression, args.rows);
         print("No output name provided. Defaulting to {}".format(output_name));
 
-    if output_name.isdigit():
-        # Need to prevent output file from starting with a digit
-        print("Output file must not begin with a number, as this will cause ironvault to fail reading the file.");
-        print("Please change the output file name and try again.");
-        exit();
-
+    validateFileName(output_name);
 
     # Take in file to extract data from, and delimiter
     dataset = read_dataset_from_file(args.input_file, args.separator, not args.no_auto_trim);
